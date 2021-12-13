c2ad0f5a4859383af57be0374800589bf64ad3a2
Gabriele Ceccolini <52239001+Gabrocecco@users.noreply.github.com>
2021-12-13 15:10:39 +0100

    Add files via upload

---
+++ Esercitazione 9/sort.h
@@ -0,0 +1,5 @@
+#include "votafattorex.h"
+
+void quickSortR(classifica *a, int start, int end);
+
+void bubbleSort(classifica *v, int n);
---
+++ Esercitazione 9/struct.h
@@ -0,0 +1,12 @@
+struct l
+{
+	char candidato[255];
+	char giudice[255];
+	char categoria;
+	char nomeFile[255];
+	char fase;
+	int voto;
+};
+typedef struct l l;
+
+#define N 10000
